9e61737dd3456ed097bb8cb340cb9c16509a450a
Jason Wright <noobjey@gmail.com>
2015-06-01 12:54:23 -0600

    vampire tests passing

--- vampire.rb
+++ vampire.rb
@@ -1,2 +1,16 @@
 class Vampire
+  attr_reader :name, :pet
+  def initialize(name, pet = 'bat')
+    @name = name
+    @pet = pet
+    @thirsty = true
+  end
+
+  def thirsty?
+    @thirsty
+  end
+
+  def drink
+    @thirsty = false
+  end
 end
